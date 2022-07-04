29041643fb51902b8016e66f55ef89e1088901b7
fantomkis <99187878+fantomkis@users.noreply.github.com>
2022-07-04 18:22:14 +0000

    Deploying to gh-pages from @ fantomkis/goit-js-hw-09@8d777e8a40fa42733b4a4eefcfcc329c94caf411 🚀

---
+++ 01-color-switcher.56e842c6.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d=null;function l(){e.disabled?(e.disabled=!1,t.disabled=!0):(e.disabled=!0,t.disabled=!1)}e.addEventListener("click",(()=>{l(),console.log(e.disabled),d=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=`${e}`}),1e3)})),t.addEventListener("click",(()=>{clearInterval(d),l()}));
+//# sourceMappingURL=01-color-switcher.56e842c6.js.map
